a20e0e2c4dc6a8a227d6465fb922803adb0f6a41
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2021-01-26 19:33:51 -0500

    9_Capacidad_Inicial
    	ArrayList(300):Se puede iniciar con un tamaño. La capacidad por defectode un ArrayList es de 10.
    LinkedList:No tiene un valor inicial

---
+++ src/main/java/com/entrenamientoJava/app/App.java
@@ -0,0 +1,14 @@
+package com.entrenamientoJava.app;
+
+import java.util.ArrayList;
+
+public class App {
+
+	public static void main(String[] args) {
+		ArrayList<String> lista = new ArrayList<>(300);
+		
+		System.out.println(lista.toString());
+
+	}
+
+}

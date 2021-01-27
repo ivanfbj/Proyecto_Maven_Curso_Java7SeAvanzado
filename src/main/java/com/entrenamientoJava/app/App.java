8a71beddd44a8bf382eeae1278b24b2e38976282
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2021-01-26 21:11:58 -0500

    10_Ordenar_Colecciones
    	-En caso de una lista dinamica se puede utilizar una Api Collections, hay una clase llamada "Collections" que contiene una serie de métodos, entre esos métodos está el "sort" que nos permite pasarle una lista como parámetro, esto permite ordenar dicha lista en orden númerico o alfabético.
    
    -El método "reverse" pone la lista al revés.

--- src/main/java/com/entrenamientoJava/app/App.java
+++ src/main/java/com/entrenamientoJava/app/App.java
@@ -1,14 +1,36 @@
 package com.entrenamientoJava.app;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class App {
 
 	public static void main(String[] args) {
-		ArrayList<String> lista = new ArrayList<>(300);
-		
-		System.out.println(lista.toString());
+		ArrayList<Integer> listaNumerica1 = new ArrayList<>();
 
+		listaNumerica1.add(25);
+		listaNumerica1.add(10000);
+		listaNumerica1.add(1);
+		Collections.sort(listaNumerica1);
+		System.out.println(listaNumerica1);
+
+		//////////////////////////////////////////////////////////////////////////////////////////////
+		ArrayList<Integer> listaNumerica2 = new ArrayList<>();
+		listaNumerica2.add(85);
+		listaNumerica2.add(65000);
+		listaNumerica2.add(15);
+		Collections.reverse(listaNumerica2);
+		System.out.println(listaNumerica2);
+
+		//////////////////////////////////////////////////////////////////////////////////////////////
+		ArrayList<String> listaString1 = new ArrayList<>();
+		listaString1.add("Jaime");
+		listaString1.add("MitoCode");
+		listaString1.add("AAA");
+		Collections.sort(listaString1);
+		System.out.println(listaString1);
+
+		
 	}
 
 }
